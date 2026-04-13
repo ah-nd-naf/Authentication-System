46f6cb739fb028919f7a66c5759036f2d47bd6a9
ah-nd-naf <ahnaf.rasheed.zaki@gmail.com>
2026-04-13 13:14:14 +0600

    Initial backend setup

---
+++ backend/server.js
@@ -0,0 +1,22 @@
+const express = require('express'); // main framework, create a server and handle requests
+const mongoose = require('mongoose');
+const cors = require('cors');  // allows React frontend to communicate with your backend
+require('dotenv').config();    // loads secret values from your .env file
+const app = express();   //  creates your actual server.
+
+app.use(cors({ origin: 'http://localhost:5173', credentials: true }));
+app.use(express.json());
+
+// Routes commented out for now
+// app.use('/api/auth', require('./routes/auth'));
+
+mongoose.connect(process.env.MONGO_URI)
+  .then(() => {
+    console.log('✅ MongoDB connected successfully');
+    app.listen(process.env.PORT, () => {
+      console.log(`✅ Server running on port ${process.env.PORT}`);
+    });
+  })
+  .catch((err) => {
+    console.log('❌ MongoDB connection failed:', err.message);
+  });
